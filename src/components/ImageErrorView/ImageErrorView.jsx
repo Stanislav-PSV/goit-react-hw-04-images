4bbbb889c8b0cf2383ebc8b8636251d722e83452
Stanislav-PSV <stanislav.psv521@gmail.com>
2022-09-19 20:32:54 +0300

    002

--- src/components/ImageErrorView/ImageErrorView.jsx
+++ src/components/ImageErrorView/ImageErrorView.jsx
@@ -1,5 +1,5 @@
 import { PropTypes } from 'prop-types';
-import errorImage from '../../images/error.jpeg'
+import errorImage from '../../images/error.jpg'
 import { ImageErrorViewStyled } from "./ImageErrorView.styled";
 
 export default function ImageErrorView({ message }) {
